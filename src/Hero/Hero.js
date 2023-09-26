2c0ca367fb945df2f93bb59771746b5194c50473
irvinvictoria <irvinvictoria@gmail.com>
2023-09-26 15:38:46 -0400

    Brought PBudget code in

---
+++ src/Hero/Hero.js
@@ -0,0 +1,12 @@
+
+function Hero() {
+    return (
+      <div className="hero" aria-details="Below the words is an image of a computer and mobile phone right next to each other">
+        <h1>Personal Budget</h1>
+        <h2>A personal-budget management app</h2>
+    </div>
+    );
+  }
+  
+  export default Hero;
+  
